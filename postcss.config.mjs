1bad1f3ff11b1f8a633906e445ad7cd3989d3d47
NikKowPHP <nik.kow@outlook.com>
2025-06-08 09:38:50 +0200

    feat: Update Next.js and remove NextAuth
    Updates Next.js to version 15.3.3 and removes the `next-auth` dependency. This change also includes updates to `postcss.config.mjs` and `src/app/globals.css` to reflect the updated Next.js configuration.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    // This tells the Next.js loader to find the plugin in the '@tailwindcss/postcss' package.
-    '@tailwindcss/postcss': {},
-    // Autoprefixer is NOT needed with Tailwind v4.
+    "@tailwindcss/postcss": {},
   },
 };
-
 export default config;
